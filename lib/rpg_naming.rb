c1fd3b53ea46c670f07fff252fb107b40fa80bc1
arihh <arihh0615@gmail.com>
2016-06-15 22:53:13 +0900

    create module RpgNaming

--- lib/rpg_naming.rb
+++ lib/rpg_naming.rb
@@ -1,5 +1,43 @@
 require "rpg_naming/version"
+require "yaml"
 
 module RpgNaming
-  # Your code goes here...
+  class Base
+    def initialize(min = 0, max = nil)
+      @min, @max = min, max
+    end
+
+    [:player, :monster, :male, :female].each do |target|
+      class_eval("def #{target}; select_range(RpgNaming.names[pluralize('#{target}')]).sample; end")
+    end
+
+    def member
+      select_range(RpgNaming.names.values_at("males", "females").flatten).sample
+    end
+
+    private
+
+    def select_range(objects)
+      objects.select do |o|
+        @min <= o.length && (@max.nil? || o.length <= @max)
+      end
+    end
+
+    def pluralize(word)
+      # FIXME
+      word.concat("s")
+    end
+  end
+
+  def self.range(min = 0, max = nil)
+    Base.new(min, max)
+  end
+
+  [:player, :monster, :member, :male, :female].each do |target|
+    module_eval("def self.#{target}; Base.new.#{target}; end")
+  end
+
+  def self.names
+    @names ||= YAML.load_file(File.expand_path(File.join('../..', 'config', 'names.yml'), __FILE__))
+  end
 end
--- spec/rpg_naming_spec.rb
+++ spec/rpg_naming_spec.rb
@@ -1,11 +1,36 @@
 require 'spec_helper'
 
 describe RpgNaming do
-  it 'has a version number' do
-    expect(RpgNaming::VERSION).not_to be nil
+  let(:names) {
+    {
+      "players" => ["ゆりしー"],
+      "monsters" => ["ゴブリン"],
+      "males" => ["ユウ"],
+      "females" => ["サツキ"],
+    }
+  }
+  before { RpgNaming.stub(:names).and_return(names) }
+  subject { RpgNaming }
+  it 'player' do
+    expect(subject.player).to eq("ゆりしー")
+  end
+  it 'monster' do
+    expect(subject.monster).to eq("ゴブリン")
+  end
+  it 'male' do
+    expect(subject.male).to eq("ユウ")
+  end
+  it 'female' do
+    expect(subject.female).to eq("サツキ")
+  end
+  it 'member' do
+    expect(subject.member).to eq("サツキ").or eq("ユウ")
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context 'range' do
+    let(:names) { {"males" => ["ユウ", "ラスク"]} }
+    it 'range' do
+      expect(subject.range(2,2).male).to eq("ユウ")
+    end
   end
 end
